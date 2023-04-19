ac15efced495f2a6b22f92437b86cbb4e57a4bd3
Moaaz Khairy <moazkhairy@example.com>
2023-04-19 14:12:11 +0200

    merged commit

--- src/main/java/com/example/universitysystem/repositories/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.universitysystem.repositories;
-
-import com.example.universitysystem.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, String> {
-
-}
--- src/main/java/com/example/universitysystem/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.universitysystem.repositories;
-
-import com.example.universitysystem.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
